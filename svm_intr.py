5ee6f8bd2902fba38a34288b55e2641f9875739e
2019code <51526832+2019code@users.noreply.github.com>
2019-06-09 16:19:47 +0800

    Update svm_intr.py

--- svm_intr.py
+++ svm_intr.py
@@ -9,4 +9,12 @@ if __name__ == "__main__":
     path = '..\\phone.data'
     data = pd.read_csv(path,header=None)
     x, y = data[range(4)], data[4]
+    y = pd.Categorical(y).codes
+    x = x[[0,1]]
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1,train_size=0.6)
+    
+    # fen lei 
+    clf = svm.SVC(C=0.1, kernel='linear', decision_function_shape='ovr')
+    clf.fit(x_train,y_train.ravel())
+    
     
